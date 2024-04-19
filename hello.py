e1610f4f8e6c03db049be6112ee5224373105de0
DahaiSun <sundahai63@gmail.com>
2024-04-20 03:43:30 +0800

    hello

---
+++ hello.py
@@ -0,0 +1,19 @@
+# name ="haha"
+# print("Hello {name} how are you".format(name=name))
+# print("Hello {0} how are you".format(name))
+# print(f"Hello {name} how are you")
+
+
+# name = input("what's your name")
+# print(f"Hello {name}, How are you?")
+
+length = input("length: ")
+width = input("width: ")
+sum = length + width
+print(f"the sum is {sum}")
+
+# sum = int(length) + int(width)
+# print(f"the sum is {sum}")
+
+sum = float(length) + float(width)
+print(f"the sum is {sum:.2f}")
---
+++ number_guess_game.py
@@ -0,0 +1,4 @@
+name ="haha"
+print("Hello {name} how are you".format(name=name))
+print("Hello {0} how are you".format(name))
+print(f"Hello {name} how are you")
