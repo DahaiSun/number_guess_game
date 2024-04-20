8dd46248d9ab3cfca41abfb6d5a768f0dc3954c0
Boyue <166930287+DahaiSun@users.noreply.github.com>
2024-04-20 23:09:07 +0800

    Update number_guess_game.py

--- day2/number_guess_game.py
+++ day2/number_guess_game.py
@@ -1,13 +1,16 @@
 import random
-random_number = random.randrange(1,21)
+random_number = random.randint(1,21)
 
+guess_time= 0
 while True:
-    guess = input("I have a number in mind between 1-20. What do you think it is? ")
+    guess = int(input("I have a int number in mind between 1-20. What do you think it is? "))
+    guess_time += 1
     if guess < random_number:
         print("too small, try again") 
     if guess > random_number:
         print("too big, try again")
     if guess == random_number:
         print("you are right")
-        
-print(number)
+        break
+    
+print(f"number of guesses: {guess_time}")
